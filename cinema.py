756240597d81cff74419782b23038d52a608bb81
fvanh <fvanhaelst@hotmail.com>
2021-11-15 21:04:55 +0100

    changes

--- cinema.py
+++ cinema.py
@@ -10,9 +10,9 @@ elif(leeftijd < 17):
     prijs = 4
 else:
     prijs = 5
-if(film3d = "ja"):
+if(film3d == "ja"):
     prijs = prijs +2
-else
+else:
     prijs = prijs + 0
 
-print("persoon 1 betaald:",kostprijs)
+print("persoon 1 betaald:",prijs)
